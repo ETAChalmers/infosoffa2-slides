41fc6b8d6b7d63864c4deaf81332a233799d1639
ertex <davidlgjohansson@gmail.com>
2022-01-12 19:47:05 +0100

    updated weather to save temp files in /tmp/

--- images/weather.bash
+++ images/weather.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl http://wd.cfab.chalmers.se/pages/station/liveData.php --silent --output data_curl.html
-file=$(echo "cat //html/body/div/div/table/tr/td/div" |  xmllint --html --shell data_curl.html)
-rm -f data_curl.html
+curl http://wd.cfab.chalmers.se/pages/station/liveData.php --silent --output /tmp/data_curl.html
+file=$(echo "cat //html/body/div/div/table/tr/td/div" |  xmllint --html --shell /tmp/data_curl.html)
+rm -f /tmp/data_curl.html
 
 #MMmMMmm RegEx
 re='([0-9.]+)([^0-9]*)([0-9.]+)([^0-9]*)([0-9.]+)([^0-9]*)([0-9.]+)([^0-9]*)([0-9.]+)([^0-9]*)([0-9.]+)([^0-9]*)([0-9.]+)([^0-9]*)'
