456395896d866c5c5514d89c181626bfb493b344
loovjo <jonathan.loov@gmail.com>
2022-01-28 14:55:13 +0100

    a.out -----> rainbow

--- slideshow.sh
+++ slideshow.sh
@@ -12,7 +12,7 @@ do
 
 		if [ $(($RANDOM%100)) -lt 1 ]
                 then
-			$interpreter "$file" | /home/pi/infosoffa2-slides/better_lolcat/a.out
+			$interpreter "$file" | /home/pi/infosoffa2-slides/better_lolcat/rainbow
 			exit_code=$?
 		else
 			$interpreter "$file"
