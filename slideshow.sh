541f945a126d87e23818ab5a3c1c09005faf786a
loovjo <jonathan.loov@gmail.com>
2022-03-02 14:36:28 +0100

    better status

--- slideshow.sh
+++ slideshow.sh
@@ -6,9 +6,16 @@ HEIGHT=$(tput lines)
 IMAGE_PATH="/home/pi/infosoffa2-slides/images"
 # IMAGE_PATH="$(pwd)/images"
 
+# head doesn't support heading 0 lines, this one does
+function head_but_good {
+    if [[ $1 -gt 0 ]] ; then
+        head -n $1
+    fi
+}
+
 while true
 do
-    IDX=1
+    IDX=0
     # TODO: fillistan splitas vid mellanslag, borde ha ett bättre sätt
     count=$(echo $IMAGE_PATH/* | wc -w | tr -d ' ')
 
@@ -21,11 +28,11 @@ do
 			interpreter=cat
 		fi
 
-        bar=$(yes "#" | head -n $IDX | tr -d '\n' ; yes "." | head -n $(($count-$IDX+1)) | tr -d '\n')
+        bar=$(yes "#" | head_but_good $(($IDX+1)) | tr -d '\n' ; yes "." | head_but_good $(($count-$IDX-1)) | tr -d '\n')
         bottom_text="$(basename $file) - [ $bar ] "
 
         w=$(echo "$bottom_text" | wc -c)
-        x=$((WIDTH-w))
+        x=$((WIDTH-w+2))
 
         printf "\x1b[$HEIGHT;${x}H$bottom_text\x1b[1;1H"
 
