6b142595bae2055d4e9247cf573f87e622b8c3b4
xenia <xenia.agda@gmail.com>
2025-11-08 15:38:04 +0100

    sussy...

--- images/spinny.sh
+++ images/spinny.sh
@@ -1,2 +1,6 @@
-infosoffa2-slides/stl-viewer/stl-viewer infosoffa2-slides/stl-viewer/kalle-micro.stl
+if [ $(($RANDOM%$5)) -lt 1 ]; then
+    infosoffa2-slides/stl-viewer/stl-viewer infosoffa2-slides/stl-viewer/kalle-micro.stl
+else
+    infosoffa2-slides/stl-viewer/stl-viewer infosoffa2-slides/stl-viewer/among.stl
+fi
 exit 100
